e2b24d805ac410216dc9bb320565d3a7859ff944
JeongHoLim <9997ijh@gmail.com>
2022-10-14 22:58:19 +0900

    feat: apply conformer-ctc

--- app/api/endpoints/voice.py
+++ app/api/endpoints/voice.py
@@ -7,16 +7,23 @@ from fastapi import APIRouter, Response, UploadFile, Form, File
 from fastapi.responses import FileResponse
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-from model import whisper
+from model import whisper, conformer
 
 logging.basicConfig(level=logging.INFO)
 router = APIRouter()
 
 
-@router.post("/whisper", response_class=FileResponse)
+@router.post("/whisper")
 def _whisper(audio_file: UploadFile = File(), answer: str = Form()):
-
     recognized_text, cer = whisper.speech_to_text(audio_file, answer)
+    print(recognized_text, cer)
+    output = {"recognized_text": recognized_text, "cer": cer}
+    return Response(content=json.dumps(output), media_type="application/json")
+
+
+@router.post("/conformer")
+def _conformer(audio_file: UploadFile = File(), answer: str = Form()):
+    recognized_text, cer = conformer.speech_to_text(audio_file, answer)
     print(recognized_text, cer)
     output = {"recognized_text": recognized_text, "cer": cer}
     return Response(content=json.dumps(output), media_type="application/json")
